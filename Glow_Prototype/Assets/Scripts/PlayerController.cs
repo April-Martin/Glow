6a2e12b5a794930362e0274ce7e7fca2e060f67d
April Martin <nandra001@gmail.com>
2016-09-23 22:40:19 -0600

    Added multi-jump functionality

--- Glow_Prototype/Assets/Scripts/PlayerController.cs
+++ Glow_Prototype/Assets/Scripts/PlayerController.cs
@@ -4,14 +4,17 @@ using Prime31;
 
 public class PlayerController : MonoBehaviour {
 
-    public float gravity = -35;
     public float jumpHeight = 2;
     public float walkSpeed = 1;
+    public float jumpsAllowed = 2;
+
+    public float gravity = -35;
 	public Transform startPos;
 	public GameObject gooPrefab;
 
     private CharacterController2D _controller;
-
+    private int jumpCounter = 0;
+    private bool isHoldingDownJump = false;
 
 	// Use this for initialization
 	void Start () {
@@ -22,14 +25,25 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        Vector3 velocity = CalculateVelocity();
+        velocity.y += gravity * Time.deltaTime;
+        _controller.move(velocity * Time.deltaTime);
+
+        if (_controller.isGrounded)
+            jumpCounter = 0;
+
+	}
+
+    Vector3 CalculateVelocity()
+    {
+        Vector3 velocity = _controller.velocity;
+        velocity.x = 0;
+
         if (Input.GetKeyDown(KeyCode.K))
         {
             DropGoo();
             KillPlayer();
         }
-
-        Vector3 velocity = _controller.velocity;
-        velocity.x = 0;
 
         if (Input.GetAxis("Horizontal") < 0)
         {
@@ -40,15 +54,21 @@ public class PlayerController : MonoBehaviour {
             velocity.x = walkSpeed;
         }
 
-        if (Input.GetAxis("Jump") > 0 && _controller.isGrounded)
+        if (Input.GetAxis("Jump") > 0 && !isHoldingDownJump && (jumpCounter < jumpsAllowed))
         {
             velocity.y = Mathf.Sqrt(2f * jumpHeight * -gravity);
+            jumpCounter++;
+            isHoldingDownJump = true;
+
         }
 
-        velocity.y += gravity * Time.deltaTime;
-        _controller.move(velocity * Time.deltaTime);
+        if (Input.GetAxis("Jump") <= 0)
+        {
+            isHoldingDownJump = false;
+        }
 
-	}
+        return velocity;
+    }
 
     private void DropGoo()
     {
