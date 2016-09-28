ee2e126b9c9ee4e288433139a7d185496abaf470
u0891819 <u0891819@kcmac-51.scl.utah.edu>
2016-09-28 16:50:20 -0600

    Made enemies compatible with moving platforms; also sped up the movement a bit, so he can jump over enemies comfortably.

--- Glow_Prototype/Assets/Scripts/EnemyController.cs
+++ Glow_Prototype/Assets/Scripts/EnemyController.cs
@@ -17,9 +17,23 @@ public class EnemyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Handle gravity
 		Vector3 velocity = _controller.velocity;
 		velocity.y += gravity * Time.deltaTime;
+
+		// Handle moving platforms
+		if (_controller.isGrounded && _controller.ground != null && _controller.ground.tag == "MovingPlatform") {
+			this.transform.parent = _controller.ground.transform;
+		} else {
+			if (this.transform.parent != null)
+				transform.parent = null;
+		}
+
+		// Apply velocity
 		_controller.move (velocity * Time.deltaTime);
+
+
 	}
 
 }
