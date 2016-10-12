526ab06cc1a9391266d186efaac809ed3618b9db
April Martin <nandra001@gmail.com>
2016-10-12 14:36:03 -0600

    Messed up the previous commit, THIS is the updated and functioning goo one!

--- Glow_Unity/Assets/Scripts/GooBehavior.cs
+++ Glow_Unity/Assets/Scripts/GooBehavior.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GooBehavior : MonoBehaviour {
+public class GooBehavior : MonoBehaviour
+{
 
     /* NOTE:
      * This code is predicated on the mask and the goo being the SAME SIZE.
@@ -10,44 +11,142 @@ public class GooBehavior : MonoBehaviour {
      */
 
     public SpriteRenderer mask;
+    public Sprite splattedGoo;
+
     private SpriteRenderer goo;
-    
+    private Rigidbody2D rb;
+    private Vector3 impactVelocity;
+    private Vector3 impactPos;
+    private Vector3 impactRotation;
+    private bool isVerticalCollision = false;
+    private bool hasCollided = false;
+    private bool hasSplatted = false;
 
-	// Use this for initialization
-	void Start () {
-        goo = GetComponent<SpriteRenderer>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-    void OnCollisionEnter2D (Collision2D collision)
+    // Use this for initialization
+    void Start()
     {
-		if (collision.collider.tag == "MovingPlatform") {
-			this.transform.parent = collision.collider.transform;
-		}
+        goo = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
 
-        GetComponent<Rigidbody2D>().isKinematic = true;
-        ContactPoint2D[] cps = collision.contacts;
-        // If part of the sprite is hanging off the platform:
-        float overlap = cps[1].point.x - cps[0].point.x;
+    }
 
-        if (overlap != goo.bounds.size.x) 
+    // Update is called once per frame
+    void Update()
+    {
+        if (!(hasCollided))
+            impactVelocity = rb.velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Handle moving platforms
+        if (collision.collider.tag == "MovingPlatform")
         {
-            float protrusion = goo.bounds.size.x - Mathf.Abs(overlap);
+            this.transform.parent = collision.collider.transform;
+        }
 
+        // Change to splatted form
+        float oldGooWidth = goo.bounds.size.x;
+        float oldGooHeight = goo.bounds.size.y;
+        goo.sprite = splattedGoo;
+        GetComponent<BoxCollider2D>().size = goo.bounds.size;
+
+
+        // Figure out the direction of the collision, and position+rotate the splash accordingly.
+        ContactPoint2D[] cps = collision.contacts;
+        impactPos = transform.position;
+        float fudgeRoom = 0.005f;
+
+        if (cps[0].point.x < cps[1].point.x + fudgeRoom && cps[0].point.x > cps[1].point.x - fudgeRoom)
+        {
+            isVerticalCollision = true;
+            Vector3 offset = new Vector3((oldGooWidth + goo.bounds.size.y) / 2, 0, 0);
+            if (impactVelocity.x > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 90);
+                transform.position += offset;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, -90);
+                transform.position -= offset;
+            }
+        }
+        else if (cps[0].point.y < cps[1].point.y + fudgeRoom && cps[0].point.y > cps[1].point.y - fudgeRoom)
+        {
+            isVerticalCollision = false;
+            Vector3 offset = new Vector3(0, (oldGooHeight + goo.bounds.size.y) / 2, 0);
+            if (impactVelocity.y > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 180);
+                transform.position += offset;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.position -= offset;
+            }
+        }
+        else
+            Debug.Log("Can't figure out the direction of the collision.");
+
+
+        hasCollided = true;
+        impactRotation = transform.eulerAngles;
+        impactPos = transform.position;
+
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (hasSplatted) return;
+
+        rb.isKinematic = true;
+        transform.position = impactPos;
+        transform.eulerAngles = impactRotation;
+        hasSplatted = true;
+
+        // Set up variables for projection calculation
+        ContactPoint2D[] cps = collision.contacts;
+        float gooSize;
+        float overlap;
+        if (isVerticalCollision)
+        {
+            gooSize = goo.bounds.size.y;
+            overlap = cps[1].point.y - cps[0].point.y;
+        }
+        else
+        {
+            gooSize = goo.bounds.size.x;
+            overlap = cps[1].point.x - cps[0].point.x;
+        }
+
+        // If part of the sprite is hanging off the platform:
+        Debug.Log("gooSize: " + gooSize);
+        Debug.Log("overlap: " + overlap);
+        if (overlap < gooSize)
+        {
+            float protrusion = gooSize - Mathf.Abs(overlap);
+            Debug.Log("protrusion: " + protrusion);
             SpriteRenderer newMask = (SpriteRenderer)Instantiate(mask, transform.position, Quaternion.identity);
             newMask.transform.parent = this.transform;
-            newMask.transform.localScale = new Vector3(protrusion / goo.bounds.size.x, 1, 1);
 
-            Vector3 offset = new Vector3(overlap / 2, 0, 0);
-            newMask.transform.position += offset; // overlap is automatically the right sign for this
-
-
+            Vector3 offset;
+            if (isVerticalCollision)
+            {
+                newMask.transform.localScale = new Vector3(1, protrusion / gooSize, 1);
+                offset = new Vector3(0, overlap / 2, 0);
+            }
+            else
+            {
+                newMask.transform.localScale = new Vector3(protrusion / gooSize, 1, 1);
+                offset = new Vector3(overlap / 2, 0, 0);
+            }
+            newMask.transform.position += offset;       // overlap is automatically the right sign for this
 
         }
 
     }
+
 }
