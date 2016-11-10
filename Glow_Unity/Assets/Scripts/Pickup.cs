d372b4e3090986221e17951057009d7fe98fc90e
April Martin <nandra001@gmail.com>
2016-11-09 20:01:51 -0700

    Got the health and goo pickups working.

---
+++ Glow_Unity/Assets/Scripts/Pickup.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+    public int amount;
+    public pickupType type;
+
+    public enum pickupType { goo, health };
+}
